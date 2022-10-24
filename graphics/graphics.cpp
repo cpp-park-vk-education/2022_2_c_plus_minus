85e5999df982d02841187d2320ec85f378364f32
Nikita Goryachev <nic.gor@yandex.ru>
2022-10-24 18:52:04 +0300

    Make cells render

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+const int WINDOW_WIDTH = 200;
+const int WINDOW_HEIGHT = 200;
+const int CELL_SIDE = WINDOW_WIDTH / 8;
 
-    while (window.isOpen())
-    {
+int main() {
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
+
+    std::array<sf::RectangleShape, 64> cells;
+    for (int i = 0; i < 64; ++i) {
+        auto& cell = cells[i];
+        int y = i / 8;
+        int x = i % 8;
+        cell.setSize({CELL_SIDE, CELL_SIDE});
+        cell.setPosition({x * CELL_SIDE, y * CELL_SIDE});
+        cell.setFillColor((x + y) % 2 ? sf::Color::Black : sf::Color::White);
+    }
+    
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+
+        for (auto& cell : cells) {
+            window.draw(cell);
+        }
+
         window.display();
     }
 
