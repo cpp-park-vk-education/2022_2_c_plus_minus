00cf91367de426c816eb960bf06d98ec770770d8
dyndtikj <vlamaster08@gmail.com>
2022-12-19 18:38:26 +0300

    fix async

--- project/client/main.cpp
+++ project/client/main.cpp
@@ -45,10 +45,10 @@ int main() {
         switch (choice) {
             case 1:
             {
-                for (int i = 0; i < 100; i++) {
-                    std::cout << i << std::endl;
+//                for (int i = 0; i < 100; i++) {
+//                    std::cout << i << std::endl;
                     client->GetAllRooms();
-                }
+//                }
                 break ;
             }
             case 2:
