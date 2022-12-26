e41100197d12800ade006e7af22ac8f5a51524a2
dyndtikj <vlamaster08@gmail.com>
2022-12-26 16:50:29 +0300

    final

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -o pipefail
-
 function print_header() {
     echo -e "\n***** ${1} *****"
 }
@@ -20,15 +19,5 @@ function check_log() {
         exit $STATUS
     fi
 }
-
-#print_header "RUN cppcheck"
-#check_log "cppcheck ./project --enable=all --inconclusive --error-exitcode=1 -Iproject/server/include -Iproject/client/include --suppress=missingIncludeSystem" "\(information\)"
-
-print_header "RUN clang-tidy"
-check_log "clang-tidy project/server/*.cpp project/client/*.cpp -warnings-as-errors=* -extra-arg=-std=c++17 -- -I project/client/include -Iproject/server/include" "Error (?:reading|while processing)"
-
-#print_header "RUN cpplint"
-#check_log "cpplint ./project/server/src/*.cpp ./project/server/*.cpp  ./project/server/include/*.h
-#        ./project/client/src/*.cpp ./project/client/*.cpp  ./project/client/include/*.h ./tests/*.cpp" "Can't open for reading"
 
 print_header "SUCCESS"
