bafa989e73194712cc7fe1efba2447bfde00a9a5
dyndtikj <vlamaster08@gmail.com>
2022-12-27 20:50:35 +0300

    fixies

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -21,14 +21,4 @@ function check_log() {
     fi
 }
 
-#print_header "RUN cppcheck"
-#check_log "cppcheck ./project --enable=all --inconclusive --error-exitcode=1 -Iproject/server/include -Iproject/client/include --suppress=missingIncludeSystem" "\(information\)"
-
-print_header "RUN clang-tidy"
-check_log "clang-tidy project/server/*.cpp project/client/*.cpp -warnings-as-errors=* -extra-arg=-std=c++17 -- -I project/client/include -Iproject/server/include" "Error (?:reading|while processing)"
-
-#print_header "RUN cpplint"
-#check_log "cpplint ./project/server/src/*.cpp ./project/server/*.cpp  ./project/server/include/*.h
-#        ./project/client/src/*.cpp ./project/client/*.cpp  ./project/client/include/*.h ./tests/*.cpp" "Can't open for reading"
-
 print_header "SUCCESS"
